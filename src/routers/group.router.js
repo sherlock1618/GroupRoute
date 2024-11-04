750ac67a04704eaa47da2a9f135e10595b671ea7
sherlock1618 <sherlock061003@gmail.com>
2024-11-04 14:56:40 +0500

    welcome page added

--- src/routers/group.router.js
+++ src/routers/group.router.js
@@ -14,5 +14,8 @@ groupsRouter.get("/get/all/admin", AuthenticateJWT, HTTPGetAllGroups);
 groupsRouter.post("/create", AuthenticateJWT, HTTPCreateGroup);
 groupsRouter.delete("/delete/:id", AuthenticateJWT, HTTPRemoveGroup);
 groupsRouter.put("/update/:id", AuthenticateJWT, HTTPUpdateGroup);
+groupsRouter.get("/", (req, res) => {
+    res.send("Welcome to the Groups API!"); // Your welcome message
+});
 
 module.exports = groupsRouter;
